75ae681a1cc4c42c1005a965473bb96016310c6b
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-10-10 20:43:23 +0300

    add

--- src/components/UserMenu/UseMenu.js
+++ src/components/UserMenu/UseMenu.js
@@ -6,11 +6,11 @@ import { logOut } from 'Redux/auth/operation';
 export const UseMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
-
+  const handleLogOut = () => dispatch(logOut());
   return (
     <div className={css.wrapper}>
       <p className={css.welcome}>Welcome, {user.name}</p>
-      <button type="button" onClick={() => dispatch(logOut())}>
+      <button type="button" onClick={handleLogOut}>
         ↪️Logout
       </button>
     </div>
